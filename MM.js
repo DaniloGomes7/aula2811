3ab15ca7df11a04f158dac1875ac2faea5dd7d4c
Danilo Gomes7 <danilo.gomes48@etec.sp.gov.br>
2024-11-28 21:30:33 -0300

    Aula 28/11

---
+++ MM.js
@@ -0,0 +1,8 @@
+var num1 = parseFloat(prompt("Digite o valor 1"))
+var num2 = parseFloat(prompt("Digite o valor 2"))
+
+if(num1 > num2 ){
+    alert("O valor maior é: " + num1)
+}else{
+    alert("O valor maior é: " + num2)
+}
